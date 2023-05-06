7c3a875f4fdbd922cd277fc1502a91d4197f5467
SWUTM <balcknutfdsfsddfsdsf@gmail.com>
2023-05-07 02:42:19 +0300

    usefulerottenflash-2.0-1.19-1.19.2

---
+++ src/main/java/net/swutm/ediblerottenflash/ediblerottenflash.java
@@ -0,0 +1,34 @@
+package net.swutm.ediblerottenflash;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ediblerottenflash.MOD_ID)
+public class ediblerottenflash {
+    // Directly reference a slf4j logger
+    public static final String MOD_ID = "ediblerottenflash";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ediblerottenflash() {
+        // Register the setup method for modloading
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getName());
+    }
+}
